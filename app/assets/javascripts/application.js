2af47b36a5da86c13e65227e8dc14af556736f14
Brandon Hays <brandon.hays@gmail.com>
2013-06-24 22:23:12 -0500

    ember basic setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require tweetlinks
+
+// for more details see: http://emberjs.com/guides/application/
+Tweetlinks = Ember.Application.create();
+
 //= require_tree .
