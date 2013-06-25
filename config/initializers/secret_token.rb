c08ef9f75534d9fd1feed3a29c1104f88d8e6d92
Brandon Hays <brandon.hays@gmail.com>
2013-06-25 00:22:34 -0500

    rake secret and move token to .env

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,3 +1,5 @@
+require 'dotenv'
+Dotenv.load
 # Be sure to restart your server when you modify this file.
 
 # Your secret key is used for verifying the integrity of signed cookies.
@@ -9,4 +11,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Tweetlinks::Application.config.secret_key_base = '537e752641c87fb2710745a8f8323ce246b1ca9a22951ba4343a40ea35fba0f186993d82d80d0866c5d48da63fb3c95aaf342ff236413f13935d025b39ab2996'
+Tweetlinks::Application.config.secret_key_base = ENV['SECRET_TOKEN']
